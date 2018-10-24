e8a59a2ae7aa840c568acff18b52fa268cf9bc6f
Eileen S <eileen.j.sul@gmail.com>
2018-10-24 12:29:26 -0500

    updated html and js pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// declare Variable
+var randomNumber="";
+var totalScore="";
+
+// initialize Game
+var wins=0;
+var losses=0;
+
+document.getElementById("showWins").textContent = wins;
+document.getElementById("showLosses").textContent = losses;
+
+// new Game function
+
+// onkeyUp
