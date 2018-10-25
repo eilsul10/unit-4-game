e5485cfcb7e40f5500111cfd271c5fb4b8b6c833
Eileen S <eileen.j.sul@gmail.com>
2018-10-25 12:35:09 -0500

    added more Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,27 @@
 // declare Variable
 var randomNumber="";
 var totalScore="";
+var gemValue1="";
+var gemValue2="";
+var gemValue3="";
+var gemValue4="";
 
 // initialize Game
 var wins=0;
 var losses=0;
+var computerGuess = gemValue1[Math.floor(Math.random() * gemValue1.length)];
+var computerGuess = gemValue2[Math.floor(Math.random() * gemValue2.length)];
+var computerGuess = gemValue3[Math.floor(Math.random() * gemValue3.length)];
+var computerGuess = gemValue4[Math.floor(Math.random() * gemValue4.length)];
+var computerGuess = randomNumber[Math.floor(Math.random() * randomNumber.length)];
 
 document.getElementById("showWins").textContent = wins;
 document.getElementById("showLosses").textContent = losses;
 
 // new Game function
+
+function newGame() {
+
+}
 
 // onkeyUp
