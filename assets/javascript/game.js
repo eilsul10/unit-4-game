036436846e9379fcb385ee888a64e6787f7de0e7
Eileen S <eileen.j.sul@gmail.com>
2018-10-27 16:17:11 -0500

    final copy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,130 @@
-window.onload = function() {
-
-    //  Click events are done for us:
-    // $("#gem1").click(gemValue1); review within parentheses
-    $("#gem2").click(gemValue2);
-    $("#gem3").click(gemValue3);
-    $("#gem4").click(gemValue4);
-  };
-
+window.onload = function () {
+    $("#gem1").click(gem1Clicked);
+    $("#gem2").click(gem2Clicked);
+    $("#gem3").click(gem3Clicked);
+    $("#gem4").click(gem4Clicked);
+};
 
 // declare Variable
-var randomNumber="";
-var totalScore="";
-var gemValue1="";
-var gemValue2="";
-var gemValue3="";
-var gemValue4="";
+var gemValue1 = Math.floor((Math.random() * 13) + 1);
+var gemValue2 = Math.floor((Math.random() * 13) + 1);
+var gemValue3 = Math.floor((Math.random() * 13) + 1);
+var gemValue4 = Math.floor((Math.random() * 13) + 1);
 
 // initialize Game
-var wins=0;
-var losses=0;
-var computerGuess = gemValue1[Math.floor(Math.random() * gemValue1.length)];
-var computerGuess = gemValue2[Math.floor(Math.random() * gemValue2.length)];
-var computerGuess = gemValue3[Math.floor(Math.random() * gemValue3.length)];
-var computerGuess = gemValue4[Math.floor(Math.random() * gemValue4.length)];
-var computerGuess = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+var computerGuess = Math.floor((Math.random() * 101) + 19);
 
 document.getElementById("showWins").textContent = wins;
 document.getElementById("showLosses").textContent = losses;
-document.getElementById("showNumber").textContent = randomNumber;
+document.getElementById("showNumber").textContent = computerGuess;
+document.getElementById("totalScore").textContent = totalScore;
 
-// new Game function
+function gem1Clicked() {
+    totalScore = Number(totalScore) + Number(gemValue1);
+    $("#totalScore").text(" " + totalScore);
+    if (computerGuess === totalScore) {
+        wins++;
+        newGame();
+        alert("You have won!!");
+    }
 
-function newGame() {
+    else if (totalScore > computerGuess) {
+        losses++;
+        newGame();
+        alert("You have lost. Please try again.");
+
+        document.getElementById("showWins").textContent = wins;
+        document.getElementById("showLosses").textContent = losses;
+        document.getElementById("totalScore").textContent = totalScore;
+
+    }
+};
+
+
+function gem2Clicked() {
+    totalScore = Number(totalScore) + Number(gemValue2);
+    $("#totalScore").text(" " + totalScore);
+
+    if (computerGuess === totalScore) {
+        wins++;
+        newGame();
+        alert("You have won!!");
+    }
+
+    else if (totalScore > computerGuess) {
+        losses++;
+        newGame();
+        alert("You have lost. Please try again.");
+
+        document.getElementById("showWins").textContent = wins;
+        document.getElementById("showLosses").textContent = losses;
+        document.getElementById("totalScore").textContent = totalScore;
+
+    }
 
 }
 
-// onkeyUp
+function gem3Clicked() {
+    totalScore = Number(totalScore) + Number(gemValue3);
+    $("#totalScore").text(" " + totalScore);
+
+    if (computerGuess === totalScore) {
+        wins++;
+        newGame();
+        alert("You have won!!");
+    }
+
+    else if (totalScore > computerGuess) {
+        losses++;
+        newGame();
+        alert("You have lost. Please try again.");
+
+        document.getElementById("showWins").textContent = wins;
+        document.getElementById("showLosses").textContent = losses;
+        document.getElementById("totalScore").textContent = totalScore;
+
+    }
+}
+
+function gem4Clicked() {
+    totalScore = Number(totalScore) + Number(gemValue4);
+    $("#totalScore").text(" " + totalScore);
+
+    if (computerGuess === totalScore) {
+        wins++;
+        newGame();
+        alert("You have won!!");
+    }
+
+    else if (totalScore > computerGuess) {
+        losses++;
+        newGame();
+        alert("You have lost. Please try again.");
+
+        document.getElementById("showWins").textContent = wins;
+        document.getElementById("showLosses").textContent = losses;
+        document.getElementById("totalScore").textContent = totalScore;
+
+    }
+
+}
+
+function newGame() {
+    totalScore= 0;
+    computerGuess = Math.floor((Math.random() * 101) + 19);
+    document.getElementById("showNumber").textContent = computerGuess;
+    document.getElementById("showWins").textContent = wins;
+    document.getElementById("showLosses").textContent = losses;
+
+}
+
+
+
+gem1Clicked();
+gem2Clicked();
+gem3Clicked();
+gem4Clicked();
+newGame();
